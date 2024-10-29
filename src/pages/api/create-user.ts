6a4edc773b28179560e8590e3455487674f3b91a
Leonel Donati <leonelroman321@gmail.com>
2024-10-29 15:40:48 -0300

    created endpoin to create a new user

--- src/pages/api/create-user.ts
+++ src/pages/api/create-user.ts
@@ -1,5 +1,21 @@
 import type { APIRoute } from "astro";
+import { validate } from "../../scripts/zod";
+import { res } from "../../scripts/helpers";
 
 export const POST: APIRoute = async ({ request }) => {
-  
-}
+  try {
+    const formData = await request.formData();
+    const avatar = await request.blob();
+
+    const payload  = Object.fromEntries(formData);
+
+    const { ok, payload: parsedPayload, issues } = validate(payload)
+
+    if (!ok) return res({ message: "Algunos datos no son válidos, por favor revísalos.", issues })
+
+    return res({payload, avatar})
+  } catch (err) {
+    console.log(err);
+    return res({ message: "Error en el servidor" }, 500);
+  }
+};
