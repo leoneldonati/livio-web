0c7f5635832c0208b9b9ade0ae49b8a2c879990a
Leonel Donati <leonelroman321@gmail.com>
2024-11-08 11:03:30 -0300

    sign in page, v0.0.1

--- src/scripts/colors.ts
+++
@@ -1,9 +0,0 @@
-export function getColorsByProvider (provider: string) {
-  if (provider === 'GITHUB') return {
-    bg: 'rgba(22,22,22,.5)',
-    border: 'rgb(22,22,22)',
-    shadow: 'rgba(22,22,22,.3)'
-  }
-  if (provider === 'INSTAGRAM') return
-  if (provider === 'GOOGLE') return
-}
