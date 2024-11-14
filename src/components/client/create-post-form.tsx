6ecab623b387b11de1755410a019c1bd92279094
Leonel Donati <leonelroman321@gmail.com>
2024-11-14 16:20:18 -0300

    COMPONENTS: create-post-form; Created client component to manage the user post and manage more reactivity

---
+++ src/components/client/create-post-form.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import ApiServices from "~/services/api";
+import { usePostStore } from "~/store";
+import Carousel from "./carousel";
+
+export default function CreatePostForm({ origin }: { origin: string }) {
+  const [assets, setAssets] = useState<string[] | null>(null);
+  const { addOnePost } = usePostStore();
+  const { createPost } = new ApiServices(origin);
+
+
+  // MANEJAR CAMBIO DE ESTADO DEL INPUT FILES
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files) {
+      const filesMap = Object.values(files);
+
+      const srcMap = filesMap.map((file) => URL.createObjectURL(file));
+
+      setAssets(srcMap);
+    }
+  };
+  return (
+    <form
+      encType="multipart/form-data"
+      className="border border-black/80 flex flex-col"
+    >
+      <label htmlFor="content">
+        <input
+          type="text"
+          name="content"
+          id="content"
+          placeholder="¿¡Qué deseas compartir?!"
+        />
+      </label>
+
+       {assets && <Carousel paths={assets} />}
+      <label
+        htmlFor="assets"
+        className="w-7 h-7 aspect-square rounded-full border border-black"
+      >
+        <input
+          type="file"
+          name="assets"
+          id="assets"
+          accept="images/*"
+          multiple
+          hidden
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="submit">Publicar</button>
+    </form>
+  );
+}
