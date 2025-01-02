2292048e5433b72ae2f15847aaf6db278961d2b1
Leonel Donati <leonelroman321@gmail.com>
2025-01-02 14:28:46 -0300

    fixed CORS

--- src/scripts/helpers.ts
+++ src/scripts/helpers.ts
@@ -2,5 +2,10 @@ type StatusResponse = 400 | 401 | 404 | 500 | null;
 export const res = (payload: object | string, status: StatusResponse = null) =>
   new Response(JSON.stringify(payload), {
     status: status ?? 200,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": import.meta.env.DOMAIN,
+      "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PATCH, DELETE",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
   });
